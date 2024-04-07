475e733e9b94a8e2166963e7f853f393f96091cc
loldonut <loldonutph@gmail.com>
2024-04-07 22:02:44 +0800

    fix missing comma in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,12 +2,13 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = function()
         require("nvim-treesitter.install").update({ with_sync = false })
-    end
+    end,
     config = function()
         require('nvim-treesitter.configs').setup({
             ensure_installed = {
                 "javascript",
                 "typescript",
+                "lua",
                 "c",
                 "vim",
                 "vimdoc"
