e87c2735a6a28eb061324da4a7fc0234db1e9133
loldonut <loldonutph@gmail.com>
2024-04-07 22:00:55 +0800

    set with_sync to false in treesitter build

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })
-    end,
+        require("nvim-treesitter.install").update({ with_sync = false })
+    end
     config = function()
         require('nvim-treesitter.configs').setup({
             ensure_installed = {
