c8d608f96154ceabe0cc270b47653b02b5dc5975
loldonut <loldonutph@gmail.com>
2024-04-07 22:35:31 +0800

    use :Git instead for pushing

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         -- MORE KEYBINDS SOON
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.api.nvim_set_keymap("n", "<leader>gp", ":Git! push<CR>", {
+        vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push<CR>", {
             noremap = true
         })
     end
