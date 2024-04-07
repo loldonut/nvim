348ba213478e37c8b2541cc3b63ec1baf5e203c5
loldonut <loldonutph@gmail.com>
2024-04-07 22:28:11 +0800

    add fugitive push keybind

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,5 +3,8 @@ return {
     config = function()
         -- MORE KEYBINDS SOON
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.api.nvim_set_keymap("n", "<leader>gp", ":Git! push", {
+            noremap = true
+        })
     end
 }
