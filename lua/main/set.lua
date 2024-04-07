535c5ea696699c21a43e03f374026f749106022e
loldonut <loldonutph@gmail.com>
2024-04-07 20:42:26 +0800

    add lazy nvim

---
+++ lua/main/set.lua
@@ -0,0 +1,25 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.updatetime = 50
+
+-- TRUE COLOR SUPPORT
+vim.opt.termguicolors = true
+
+-- GRUV BOX
+vim.cmd[[ colorscheme nightfox ]]
+vim.cmd[[ syntax on ]]
