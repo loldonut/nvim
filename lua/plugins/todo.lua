8ac80cfc49ae40b27129ae7e431a1567c518bb33
loldonut <loldonutph@gmail.com>
2024-04-08 17:35:50 +0800

    setup todo plugin

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,6 +1,8 @@
 return {
     "folke/todo-comments.nvim",
     config = function()
+        require("todo-comments").setup({})
+
         vim.keymap.set("n", "]t", function()
             require("todo-comments").jump_next()
         end, { desc = "Next todo comment!" })
@@ -8,5 +10,13 @@ return {
         vim.keymap.set("n", "[t", function()
             require("todo-comments").jump_prev()
         end, { desc = "Previous todo comment!" })
+
+        vim.keymap.set("n", "<leader>tdq", ":TodoQuickFix<CR>",
+            { desc = "Quickfix to show all todos" }
+        )
+
+        vim.keymap.set("n", "<leader>tdf", ":TodoTelescope<CR>",
+            { desc = "Find TODOs using telescope.nvim" }
+        )
     end
 }
