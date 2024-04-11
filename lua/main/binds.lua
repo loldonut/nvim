a6a34f965bafeec5b3c0bb0a1590bbb3ca2e681b
loldonut <loldonutph@gmail.com>
2024-04-11 19:50:10 +0800

    add nvim surround

--- lua/main/binds.lua
+++ lua/main/binds.lua
@@ -1,19 +1,19 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set("n", '<leader>pv', vim.cmd.Ex)
 
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 
-vim.keymap.set('x', '<leader>p', '"_dP"')
+vim.keymap.set('x', "<leader>p", '"_dP"')
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set("n", '<C-d>', '<C-d>zz')
+vim.keymap.set("n", '<C-u>', '<C-u>zz')
 
 
-vim.keymap.set('n', '<leader>fd', 'd2f/')
-vim.api.nvim_set_keymap('n', '<leader>fc', 'gcc', { noremap = false })
+vim.keymap.set("n", "<leader>fd", "d2f/")
+vim.api.nvim_set_keymap('n', "<leader>fc", 'gcc', { noremap = false })
 vim.api.nvim_set_keymap('v', '<leader>fc', 'gcc', { noremap = false })
---
+++ lua/plugins/surround.lua
@@ -0,0 +1,9 @@
+return {
+    "kylechui/nvim-surround",
+    config = function()
+        require("nvim-surround").setup({})
+
+        vim.keymap.set("n", "<leader>sqd", "ysw\"")
+        vim.keymap.set("n", "<leader>cqd", "ys$\"")
+    end
+}
