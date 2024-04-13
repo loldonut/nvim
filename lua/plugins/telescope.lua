4a0819cf5e4a429d35055b1d54a601c1aeee8102
loldonut <loldonutph@gmail.com>
2024-04-13 23:39:13 +0800

    feat(telescope): add keymap for `lsp_references`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,18 +3,20 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     tag = "0.1.6",
     config = function()
-        local builtin = require('telescope.builtin')
+        local builtin = require("telescope.builtin")
 
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fd', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>fd", builtin.buffers, {})
+        vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string({
-                search = vim.fn.input('Grep > ')
+                search = vim.fn.input("Grep > ")
             })
         end)
 
-        require('telescope').setup {
+        vim.keymap.set("n", "<leader>pd", builtin.lsp_references, {})
+
+        require("telescope").setup {
             pickers = {
                 buffers = {
                     show_all_buffers = true,
@@ -22,7 +24,7 @@ return {
                     previewer = false,
                     mappings = {
                         i = {
-                            ['<c-d>'] = 'delete_buffer',
+                            ["<c-d>"] = "delete_buffer",
                         }
                     },
                 }
