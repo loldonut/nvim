70d2ae33a98d4df9465f521a7f0e0ae650ae6c45
loldonut <loldonutph@gmail.com>
2024-04-16 08:38:19 +0800

    feat: add vscode snips

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,6 +82,9 @@ return {
         })
 
         local cmp = require("cmp")
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
         cmp.setup({
             snippet = {
                 expand = function(args)
