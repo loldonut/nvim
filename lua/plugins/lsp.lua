5a97fbda53ee99db4e97b8911fe96ea1f619452f
John Heinrich <loldonutph@gmail.com>
2026-07-04 14:05:28 +0800

    add open diagnostic bind

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -106,8 +106,8 @@ return {
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>dr", vim.lsp.buf.rename, opts)
         vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>dr", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>rd", vim.lsp.buf.rename, opts)
       end,
     })
